8cb9b34e71b7034c1ce400760cfca221ac8138de
DCR430 <daniel.c.reyes30@gmail.com>
2021-02-27 10:33:27 -0500

    factorial ex

--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -1,2 +1,7 @@
 //wtite a function factorial
 //factorial accepts a number and returns the factorial of that number
+
+//factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040
