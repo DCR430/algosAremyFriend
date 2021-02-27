2f56ebb9f63a30280dcfb2d89c468907d05b76e7
DCR430 <daniel.c.reyes30@gmail.com>
2021-02-27 10:40:19 -0500

    factorial code

--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -5,3 +5,11 @@
 // factorial(2) // 2
 // factorial(4) // 24
 // factorial(7) // 5040
+
+
+factorial=(number)=>{
+    if (number === 0) return 1
+    return number * factorial(number -1)
+  
+  
+  }
